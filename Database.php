d6167ef9e4609ae05adcb49e3689f4391bb3812d
dannykl <danielniguse8@gmail.com>
2018-03-12 20:55:33 +0000

    security

--- Database.php
+++ Database.php
@@ -1,6 +1,6 @@
 <?php
 	//database credentials are stored on configuration.ini file that is located in root folder
 	$configuration = parse_ini_file('../../configuration.ini'); 
-    $db = mysqli_connect($configuration['servername'],$configuration['username'],$configuration['password'],$configuration['dbname']) or die("Could not connect to database");
+	$db = mysqli_connect($configuration['servername'],$configuration['username'],$configuration['password'],$configuration['dbname']) or die("Could not connect to database");
 
 ?>
