1f5595bed84bb8d18c6772bff7636bde20ca0db9
dannykl <danielniguse8@gmail.com>
2018-03-12 20:46:46 +0000

    security

--- Database.php
+++ Database.php
@@ -4,6 +4,7 @@
 	//second username for database
 	//third parameter password to access to database
 	//fourth parameter database name
-	$db = mysqli_connect("localhost","root","","Blog") or die("Could not connect to database");
+	$configuration = parse_ini_file('../private/configuration.ini'); 
+    $db = mysqli_connect($configuration['servername'],$configuration['username'],$configuration['password'],$configuration['dbname']) or die("Could not connect to database");
 
 ?>
